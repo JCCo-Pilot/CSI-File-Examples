2eddf47522d40f9735ce79b0821308104dc62076
Jasper Chen <jdchen2017@gmail.com>
2024-09-07 08:21:01 -0500

    weird loop examples

---
+++ moreExamples.py
@@ -0,0 +1,11 @@
+import sys
+list = [1,2,3,4]
+for content,index in enumerate(list):
+    #for loops with indices
+    print("Content:"+str(content)+", Index:"+str(index))
+    pass
+for content in list:
+    #regular for each loop
+    pass
+for x in range(10):
+    print(x)
