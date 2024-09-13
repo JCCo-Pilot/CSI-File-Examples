ec0a1c1098c2dc8aac55d4c406c5d849dfed1c73
Jasper <jdchen2017@gmail.com>
2024-09-13 07:32:08 -0500

    too easy

--- TestExample.py
+++ TestExample.py
@@ -1,7 +1,15 @@
 import sys
+intake = input()
+print (int(intake))
+total = 0
+counter = 0
+while counter<=int(intake):
+    total = counter+total
+    counter = counter+1
+print (total)
 #intake = input()
 #intake2 = input()
 #print(intake)
 #print(intake2)
-for line in sys.stdin:
-    print(line)
+#for line in sys.stdin:
+    #print(line)
